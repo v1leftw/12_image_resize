45edec66134adbfbfb935abd63562ad4ab9ff7b8
v1leftw <dmitry.g.fake@gmail.com>
2018-11-06 20:03:39 +0300

    [+] arguments validating in another way [*] fixed bugs with calculations

--- image_resize.py
+++ image_resize.py
@@ -1,41 +1,62 @@
 from PIL import Image
 import argparse
-import re
+import os
 
 
-class CheckPossibilityToScale(argparse.Action):
-    def __call__(self, parser, namespace, values, option_string=None):
-        print(namespace)
-        if namespace.height is not None or namespace.width is not None:
-            parser.error("Can not scale image, cause width "
-                         "or height has been set")
-        else:
-            # In order if user specifies params after scale call
-            print("Scale has been set. Width and height will be ignored")
-            namespace.scale = values
+def validate_path(path):
+    if os.path.exists(path):
+        return path
+    else:
+        raise argparse.ArgumentTypeError("Path does not exists")
 
 
 def get_args():
     parser = argparse.ArgumentParser(description="Enter the path:")
-    parser.add_argument("-path", required=True, help="Path to file")
-    parser.add_argument("-height", help="Height of output image")
-    parser.add_argument("-width", help="Width of output image")
+    parser.add_argument(
+        "-path",
+        required=True,
+        help="Path to file",
+        type=validate_path
+    )
+    parser.add_argument(
+        "-height",
+        help="Height of output image",
+        type=int
+    )
+    parser.add_argument(
+        "-width",
+        help="Width of output image",
+        default=None,
+        type=int
+    )
     parser.add_argument(
         "-scale",
         help="Scale of output image",
-        action=CheckPossibilityToScale
+        default=None,
+        type=float
     )
-    parser.add_argument("-output", help="Path that stores output image")
-    return parser.parse_args()
+    parser.add_argument(
+        "-output",
+        help="Directory that stores output image",
+        type=validate_path
+    )
+    args = parser.parse_args()
+    if args is None:
+        parser.error("No arguments specified")
+    if args.scale and (args.height or args.width):
+        parser.error("Can not use both scale and height or width")
+    if checker(args.scale) or checker(args.width) or checker(args.heigth):
+        parser.error("Arguments can not be less or equal to zero")
+    if args.scale and args.height and args.width is None:
+        parser.error("No resize arguments specified")
+    return args
 
 
-def validate_arguments(args):
-    try:
-        int(args.scale)
-        int(args.width)
-        int(args.height)
-    except ValueError:
-        return None
+def checker(value):
+    if value > 0 or value is None:
+        return False
+    else:
+        return True
 
 
 def open_image(path_to_image):
@@ -49,45 +70,69 @@ def open_image(path_to_image):
 def resize_image(image, width, height):
     user_image_height, user_image_width = image.size
     image_proportions = user_image_width/user_image_height
-    if image_proportions != width/height:
-        print("Proportions of given image and output image are not equal")
     if width is None:
-        return image.resize((height, user_image_width * image_proportions))
+        return image.resize((
+            height,
+            round(user_image_width * image_proportions)
+        ))
     if height is None:
-        return image.resize((user_image_height * image_proportions, width))
+        return image.resize((
+            round(user_image_height * image_proportions),
+            width
+        ))
     return image.resize((height, width))
 
 
 def rescale_image(image, scale):
     height, width = image.size
-    scale = int(scale)
-    return image.resize((height * scale, width * scale))
+    return image.resize((round(height * scale), round(width * scale)))
 
 
-def generate_output_path(path, size):
+def generate_output_path(original_path, size, new_path=None):
     height, width = size
-    return re.sub(r"\.", "__{}x{}.".format(height, width), path)
+    ext = os.path.splitext(os.path.abspath(original_path))
+    if new_path is None:
+        return "{}__{}x{}{}".format(
+            original_path.split(".")[0],
+            width,
+            height,
+            ext[1]
+        )
+    filename = os.path.basename(original_path)
+    return "{}{}__{}x{}{}".format(
+        new_path.split(".")[0],
+        filename.split(".")[0],
+        width,
+        height,
+        ext[1]
+    )
 
 
 def save_image(image, path):
     try:
         image.save(path)
-        print("Image saved. Path to file: {}".format(path))
+        return True
     except IOError:
-        print("Could not save image")
+        return False
+
+
+def check_proportions(image, height, width):
+    _image_height, _image_width = image.size
+    if round(_image_width/image_height, 2) != round(width/height, 2):
+        return True
 
 
 if __name__ == "__main__":
     arguments = get_args()
-    if validate_arguments(arguments) is None:
-        exit("Argument was specified in wrong format")
     user_image = open_image(arguments.path)
-    image_height, image_width = user_image.size
     if user_image is None:
         exit("Can not open image")
+    image_height, image_width = user_image.size
     if arguments.scale is not None:
         modified_image = rescale_image(user_image, arguments.scale)
     else:
+        if check_proportions(user_image, arguments.height, arguments.width):
+            print("Proportions of given image and output image are not equal")
         modified_image = resize_image(
             user_image,
             arguments.width,
@@ -97,7 +142,13 @@ if __name__ == "__main__":
         output_path = generate_output_path(arguments.path, modified_image.size)
     else:
         output_path = generate_output_path(
-            arguments.output,
-            modified_image.size
+            arguments.path,
+            modified_image.size,
+            arguments.output
         )
-    save_image(modified_image, output_path)
+    print(output_path)
+    if save_image(modified_image, output_path):
+        print("Image saved. Path to file: {}".format(output_path))
+    else:
+        print("Could not save image")
+    user_image.close()
